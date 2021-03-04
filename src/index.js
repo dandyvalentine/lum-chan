89876e89d531450155b3e8b2b03e6efa772b8a7c
Allan D. Boswell <79388675+dandyvalentine@users.noreply.github.com>
2021-03-04 07:02:07 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-const utils = require('./utils/settings');
+/**
+ * @file Entry point for application.
+ * @author Allan D. Boswell
+ * @license GPL-3.0
+ * @version 1.0.0
+ * @description Enables application connectivity.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+const RADIO = require('./services/radio');
+const UTILS = require('./utils/settings');
 
 const { Client } = require('discord.js');
-const YTDL = require('ytdl-core-discord');
-
 const CLIENT = new Client();
 
-CLIENT.on('ready', async () => {
-  try {
-    let channel = CLIENT.channels.cache.get(utils.dandy_guild_voice_channel_id()) || (await CLIENT.channels.fetch(utils.dandy_guild_voice_channel_id()));
-
-    if (!channel) {
-      return;
-    } else {
-      let connection = await channel.join();
-
-      connection.play(await YTDL('https://www.youtube.com/watch?v=0XkZ-3RvC4A'), { type: 'opus' });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+CLIENT.once('ready', () => {
+  RADIO.start(CLIENT);
 });
 
-CLIENT.login(utils.token());
+CLIENT.login(UTILS.token());
