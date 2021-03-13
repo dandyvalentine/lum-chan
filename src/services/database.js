09faeb4af748b86b85ecd712ecbe78f454b36926
dandyvalentine <79388675+dandyvalentine@users.noreply.github.com>
2021-03-13 06:57:30 -0600

    Update database.js

---
+++ src/services/database.js
@@ -0,0 +1,30 @@
+/**
+ * @file Primary database service for application.
+ * @author Allan D. Boswell
+ * @license GPL-3.0
+ * @version 2.0.0
+ * @description The primary database service for the application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+'use strict';
+
+// Configuration libraries.
+const environment = require('../configurations/environment');
+const configuration = require('../configurations/configuration');
+
+const mongoose = require('mongoose');
+
+// Database models.
+const Configuration = require('../models/configuration.model');
