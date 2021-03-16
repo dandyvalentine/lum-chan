d507060fe7b0d9494233d4624382ed1650246407
dandyvalentine <79388675+dandyvalentine@users.noreply.github.com>
2021-03-16 09:04:55 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// All available configuration libraries for application.
+const environment = require('./configurations/environment');
+
+// All available utility libraries for application.
+const Logger = require('./utils/logger');
+
+// Command manager for application.
+const manager = require('./commands/manager');
+
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.once('ready', () => {
+  // Configure the command manager.
+  manager.init(Client, Logger);
+});
+
+Client.on('message', async (message) => {
+  manager.handle(message);
+});
+
+Client.login(environment.get_token());
